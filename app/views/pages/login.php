110dbeff98b772e0f43ce2d68df91295a3c5edb9
DinithKumudika <dinithwalpitagama@gmail.com>
2022-11-01 22:18:21 +0530

    client side validation for login added

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -6,37 +6,27 @@
                <img src="<?php echo URL_ROOT; ?>/images/login.jpg" alt="" class="login-img">
           </div>
           <div class="login-container">
-               <form class="form login-form">
+               <form class="form login-form" method="POST" id="login-form">
                     <h2>Log In</h2>
                     <div class="form-group">
-                         <!-- <div class="icon">
-                              <i class="fas fa-user"></i>
-                         </div> -->
                          <div class="input-group">
-                              <input type="text" name="" id="" class="form-control">
-                              <h5>Username</h5>
+                              <input type="text" name="" id="input-uname" class="form-control">
+                              <h5 class="">Username</h5>   
                          </div>
+                         <p class="err-uname err-login"></p>
                     </div>
                     <div class="form-group">
-                         <!-- <div class="icon">
-                              <i class="fas fa-lock"></i>
-                         </div> -->
                          <div class="input-group">   
                               <input type="password" name="" id="input-pwd" class="form-control">
-                              <h5>Password</h5>
+                              <h5 class="">Password</h5>
                               <i class="fa-solid fa-eye show-pwd-icon" id="show-pwd"></i>
                          </div>
+                         <p class="err-pwd err-login"></p>
                     </div>
-                    <!-- <div class="form-group">
-                         <div class="show-pwd-check row">
-                              <input type="checkbox" name="" id="">
-                              <h5>Show Password</h5>
-                         </div>
-                    </div> -->
                     <div class="forgot-pwd">
                          <a href="">Forgot Password?</a>
                     </div>
-                    <input type="submit" class="btn login-btn" value="login">
+                    <input type="submit" class="btn login-btn" value="login" id="btn-login">
                </form>
           </div>
      </div>
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,10 @@
 const showPasswordBtn = document.getElementById('show-pwd');
+const usernameInput = document.getElementById('input-uname');
 const passwordInput = document.getElementById('input-pwd');
+const usernameError = document.querySelector('.err-uname');
+const passwordError = document.querySelector('.err-pwd');
+const loginBtn = document.getElementById('btn-login');
+const loginForm = document.getElementById('login-form');
 let isShowing = false;
 
 // show password option
@@ -18,4 +23,66 @@ showPasswordBtn.addEventListener('click',()=>{
      }
 });
 
+
+/* stop default behavior of login form on submit */
+loginForm.addEventListener('submit',(e)=>{
+     e.preventDefault();
+});
+
 //input validation
+loginBtn.addEventListener('click',()=>{
+     
+     validate();
+});
+
+function validate(){
+     usernameValidate();
+     passwordValidate();
+}
+
+/* check for empty input fields */
+function emptyInput(inputField){
+     if(inputField.value.trim() === ''){
+          return true;
+     }
+     else{
+          return false;
+     }
+}
+
+function validInputLength(inputField){
+     if(inputField.value.length>=8 && inputField.value.length<=16){
+          return true;
+     }
+     else{
+          return false;
+     }
+}
+
+function error(inputField,element,message){
+     inputField.style.borderBottom = "2px solid #DC3545";
+     element.innerHTML = message;
+}
+
+function success(inputField,element,message){
+     inputField.style.borderBottom = "2px solid #28A745";
+     element.innerHTML = message;
+}
+
+function usernameValidate(){
+     if(emptyInput(usernameInput)){
+          error(usernameInput, usernameError, "username cannot be empty");
+     }
+     else{
+          success(usernameInput, usernameError,"");
+     }
+}
+
+function passwordValidate(){
+     if(emptyInput(passwordInput)){
+          error(passwordInput , passwordError, "password cannot be empty");
+     }
+     else{
+          success(passwordInput, passwordError,"");
+     }
+}
