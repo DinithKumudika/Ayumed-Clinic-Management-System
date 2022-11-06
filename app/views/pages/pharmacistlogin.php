c210d8492bacb1295228c50d64d9f9ca70f46a6c
Jayani Ranasinghe <jayaniranasinghe98@gmail.com>
2022-11-06 15:53:14 +0530

    changes added

---
+++ app/views/pages/pharmacistlogin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="login.css" />
+    <title>login page</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="grid-container">
+            <div class="item1 grid-row">
+                <form action="login.php" method="post" id="form" class="login-form grid-row">
+                    <img src="images/logo.png" alt="logo" class="logo">
+                    <div class=" form-content">
+                        <h1 class="topic">Pharmacist Login</h1>
+                        <h4>Username</h4>
+                        <input type="text" id="username" class="form-input" placeholder="username" />
+                        <p id="username_error"></p>
+                    </div>
+                    <div class=" form-content">
+                        <h4>Password</h4>
+                        <input type="password" id="password" class="form-input" placeholder="password" />
+                        <p id="password_error"></p>
+                    </div>
+
+                    <div class="checkbox">
+                        <input type="checkbox" id="rememebr_me" value="remember" /><label>Remember me</label> <input
+                            type="checkbox" id="show_password" onclick="myFunction()" value="show" /><label>Show
+                            password</label>
+                    </div>
+
+                    <div>
+                        <input type="submit" class="login-btn" value="Log in" id="login-btn" onclick="validate()" />
+                    </div>
+                    <div class="link forgot-password"><a href="reset password">Forgot password?</a></div>
+                    <div class="link sign-up">Not a member?<a href="signup.php">Sign up</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="login.js"></script>
+</body>
+
+</html>
