a035f902c170c12c3100635b9e04336630bf5ddc
DinithKumudika <dinithwalpitagama@gmail.com>
2022-11-18 16:12:42 +0530

    verification page/ signup backend added

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -20,10 +20,8 @@ class User extends BaseController{
      public function login_doctor(){
 
           if(Request::isPost()){
-
-               foreach ($_POST as $key => $value) {
-                    $_POST[$key] = strip_tags($value);
-               }
+               //filter_input_array(INPUT_POST,FILTER_SANITIZE_SPECIAL_CHARS);
+               Request::removeTags();
 
                $data = [
                     'username'=>trim($_POST['username']),
@@ -31,17 +29,15 @@ class User extends BaseController{
                     'error'=> ''
                ];
 
-               if(!empty($data['username']) && !empty($data['password'])){
-                    $userLoggedIn = $this->userModel->login($data['username'], $data['password']);
+               $userLoggedIn = $this->userModel->login($data['username'], $data['password']);
 
-                    if($userLoggedIn){
-                         $this->createUserSession($userLoggedIn);
-                         Url::redirect('doctor/index');  
-                    }
-                    else{
-                         $data['error'] = "invalid username or password";
+               if($userLoggedIn){
+                    $this->createUserSession($userLoggedIn);
+                    Url::redirect('Doctor/index');  
+               }
+               else{
+                    $data['error'] = "invalid username or password";
                          
-                    }
                }
           }
           else{
@@ -57,7 +53,8 @@ class User extends BaseController{
 
      public function login_pharm(){
           if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == "post"){
-               filter_input_array(INPUT_POST,FILTER_SANITIZE_SPECIAL_CHARS);
+               
+               Request::removeTags();
 
                $data = [
                     'username'=>trim($_POST['username']),
@@ -70,7 +67,7 @@ class User extends BaseController{
 
                     if($userLoggedIn){
                          $this->createUserSession($userLoggedIn);
-                         Url::redirect('pharmacist/index');  
+                         Url::redirect('Pharmacist/index');  
                     }
                     else{
                          $data['error'] = "invalid username or password";
@@ -100,28 +97,82 @@ class User extends BaseController{
           
      }
 
+     public function register_patient(){
+
+          if(Request::isPost()){
+               Request::removeTags();
+
+               $data = [
+                    'first_name'=>trim($_POST['fist-name']),
+                    'last_name'=>trim($_POST['last-name']),
+                    'dob'=>trim($_POST['dob']),
+                    'gender'=>trim($_POST['gender']),
+                    'nic'=>trim($_POST['nic']),
+                    'martial-status'=>trim($_POST['martial-status']),
+                    'address'=>trim($_POST['address']),
+                    'email'=>trim($_POST['email']),
+                    'phone'=>trim($_POST['phone']),
+                    'username'=>trim($_POST['username']),
+                    'password'=>trim($_POST['password']),
+                    'error'=> ''
+               ];
+
+               $isExistingUser = $this->userModel->isUserExists($data['username'],$data['password']);
+
+               if($isExistingUser){
+                    $data['error'] = 'user already exists';
+               }
+               else{
+                    $data['password'] = Crypto::createHash($data['password']);
+                    if($this->userModel->register($data, 1)){
+                         $age = Generate::age($data['dob']);
+                         $OTPCode = Generate::verificationCode($data['email']);
+
+                         $userId = $this->userModel->getUserId(1);
+                         $regNo = Generate::regNo($userId);
+                         $this->userModel->registerPatient($data, $age, $regNo, $OTPCode);
+                         Url::redirect('user/verify');
+                    }
+               }
+          }
+          else{
+               $data = [
+                    'first_name'=>'',
+                    'last_name'=>'',
+                    'dob'=>'',
+                    'gender'=>'',
+                    'nic'=>'',
+                    'martial-status'=>'',
+                    'address'=>'',
+                    'email'=>'',
+                    'phone'=>'',
+                    'username'=>'',
+                    'password'=>'',
+                    'error'=> ''
+               ];
+          }
+          $this->view('pages/patientRegister',$data);
+     }
+
      public function createUserSession($user){
+          Session::init();
           Session::set('user_id',$user->user_id);
           Session::set('username',$user->username);
           Session::set('role_id',$user->role_id);
      }
 
-     public function register_patient(){
-          if(Request::isPost()){
-               
-          }
-          $this->view('pages/patientRegister');
-     }
-
      public function logout(){
-
-          if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == "post"){
+          if(Request::isPost()){
                Session::unset('user_id');
                Session::unset('username');
                Session::unset('role_id');
                Session::destroy();
                Url::redirect('user/login_doctor');
           } 
+     }
+
+     public function verify(){
+          $this->view('pages/signupVerification');
      }
 
      public function error(){
